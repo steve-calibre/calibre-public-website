97040ed2fb5f0f95ded066abe3f271eab2595c16
Steve Thomas <steve@calibre.ac>
2025-10-28 00:58:29 +0000

    added analytics

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { injectSpeedInsights } from '@vercel/speed-insights';
+import { inject } from '@vercel/analytics';
  
+inject();
 injectSpeedInsights();
 
 createRoot(document.getElementById('root')).render(
